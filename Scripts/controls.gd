70036b3d8a976cc76a90b386499708a42bcdd81a
Thomas Rupp <tmrupp@umich.edu>
2026-04-19 17:12:39 -0700

    Adjust x-axis angle clamp for camera zoom and comment out debug prints in controls

--- Scripts/controls.gd
+++ Scripts/controls.gd
@@ -21,7 +21,7 @@ func _ready() -> void:
 	left_click_connect($"ControlCenter/Switching Ships Button_001/StaticBody3D", change_ship_screen.bind(false))
 
 	for idx in range(buttons.size()):
-		print("%d: Connecting button: %s" % [idx, buttons[idx]])
+		#print("%d: Connecting button: %s" % [idx, buttons[idx]])
 		var n = "ControlCenter/%s/StaticBody3D" % buttons[idx]
 		if has_node(n):
 			left_click_connect(get_node(n), grid_button_pressed.bind(idx))
@@ -96,7 +96,7 @@ func left_click_connect(node : Node, callable : Callable):
 	if node == null:
 		return
 
-	print("connecting left click for node: %s" % node.name)
+	#print("connecting left click for node: %s" % node.name)
 		
 	node.input_event.connect(
 		func(_camera, event, _event_position, _normal, _shape_idx):
